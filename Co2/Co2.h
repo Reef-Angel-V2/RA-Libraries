3418094ea8f5dbaaee6aea3576e6817ee2406da2
brennyn21 <84816265+brennyn21@users.noreply.github.com>
2025-01-27 02:11:08 -0700

    Update Co2.h

--- Co2/Co2.h
+++ Co2/Co2.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Reef Angel / Brenny Cutler
+ * Copyright 2024 Reef Angel / Brennyn Cutler
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,36 +22,14 @@
 #include <Arduino.h>
 //Basic Commands
 #define SCD4x_COMMAND_START_PERIODIC_MEASUREMENT              0x21b1
-#define SCD4x_COMMAND_READ_MEASUREMENT                        0xec05 // execution time: 1ms
-#define SCD4x_COMMAND_STOP_PERIODIC_MEASUREMENT               0x3f86 // execution time: 500ms
+#define SCD4x_COMMAND_READ_MEASUREMENT                        0xec05 
+#define SCD4x_COMMAND_STOP_PERIODIC_MEASUREMENT               0x3f86 
+#define SCD4x_COMMAND_SET_AUTOMATIC_SELF_CALIBRATION_ENABLED  0x2416 
+#define SCD4x_COMMAND_PERFORM_SELF_TEST                       0x3639 
+#define SCD4x_COMMAND_PERFORM_FACTORY_RESET                   0x3632 
+#define SCD4x_COMMAND_GET_DATA_READY_STATUS                   0xE4B8
 
-//On-chip output signal compensation
-#define SCD4x_COMMAND_SET_TEMPERATURE_OFFSET                  0x241d // execution time: 1ms
-#define SCD4x_COMMAND_GET_TEMPERATURE_OFFSET                  0x2318 // execution time: 1ms
-#define SCD4x_COMMAND_SET_SENSOR_ALTITUDE                     0x2427 // execution time: 1ms
-#define SCD4x_COMMAND_GET_SENSOR_ALTITUDE                     0x2322 // execution time: 1ms
-#define SCD4x_COMMAND_SET_AMBIENT_PRESSURE                    0xe000 // execution time: 1ms
 
-//Field calibration
-#define SCD4x_COMMAND_PERFORM_FORCED_CALIBRATION              0x362f // execution time: 400ms
-#define SCD4x_COMMAND_SET_AUTOMATIC_SELF_CALIBRATION_ENABLED  0x2416 // execution time: 1ms
-#define SCD4x_COMMAND_GET_AUTOMATIC_SELF_CALIBRATION_ENABLED  0x2313 // execution time: 1ms
-
-//Low power
-#define SCD4x_COMMAND_START_LOW_POWER_PERIODIC_MEASUREMENT    0x21ac
-#define SCD4x_COMMAND_GET_DATA_READY_STATUS                   0xe4b8 // execution time: 1ms
-
-//Advanced features
-#define SCD4x_COMMAND_PERSIST_SETTINGS                        0x3615 // execution time: 800ms
-#define SCD4x_COMMAND_GET_SERIAL_NUMBER                       0x3682 // execution time: 1ms
-#define SCD4x_COMMAND_PERFORM_SELF_TEST                       0x3639 // execution time: 10000ms
-#define SCD4x_COMMAND_PERFORM_FACTORY_RESET                   0x3632 // execution time: 1200ms
-#define SCD4x_COMMAND_REINIT                                  0x3646 // execution time: 20ms
-#define SCD4x_COMMAND_GET_FEATURE_SET_VERSION                 0x202F // execution time: 1ms
-
-//Low power single shot - SCD41 only
-#define SCD4x_COMMAND_MEASURE_SINGLE_SHOT                     0x219d // execution time: 5000ms
-#define SCD4x_COMMAND_MEASURE_SINGLE_SHOT_RHT_ONLY            0x2196 // execution time: 50ms
 
 #define I2CCo2          0x62
 
@@ -59,7 +37,7 @@
 class Co2Sensor {
 public:
     Co2Sensor();
-    bool begin(bool startMeasurements); 
+    bool begin(bool startMeasurements);
     bool readMeasurement();
     bool performFactoryReset();
     int getCO2Level();
@@ -70,8 +48,11 @@ public:
     int co2 = 0;
     int temperature = 0;
     int co2Humidity = 0;
-    bool readRegister(uint16_t registerAddress, uint16_t *response, uint16_t delayMillis = 1);
+    
+    
 private:
+    bool readRegister(uint16_t registerAddress, uint16_t* response, uint16_t delayMillis = 1);
+    bool initialized = false;
     bool writeCommand(uint16_t command);
     bool readData(uint8_t* data, uint8_t len);
     void resetValues();
@@ -82,3 +63,5 @@ private:
 
 
 #endif
+
+
